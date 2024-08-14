e58261fc6eb8a17d5b3b6d505fe9c43af74684ce
TomoKanazawa <itstomohiro@gmail.com>
2024-08-14 14:16:35 +0900

    Create Next JS app with Nextra

---
+++ theme.config.jsx
@@ -0,0 +1,64 @@
+// Nextra configuration file
+
+import Image from "next/image";
+
+export default {
+  logo: (
+    <div style={{ display: "flex", alignItems: "center" }}>
+      <Image
+        style={{ display: "inline-block", margin: "5px", paddingBottom: "2px" }}
+        src="/images/mainIcon.png"
+        alt="OneNode logo"
+        width={25}
+        height={25}
+      />
+      <b>OneNode API</b>
+    </div>
+  ),
+  project: {
+    link: "https://onenode.ai/contact",
+  },
+  editLink: {
+    component: null,
+  },
+  sidebar: {
+    toggleButton: true,
+    titleComponent({ title, type }) {
+      if (title === "Python") {
+        return (
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Image
+              style={{ display: "inline-block", margin: "4px" }}
+              src="/images/python-icon.svg"
+              alt="python logo"
+              width={15}
+              height={15}
+            />
+            <span style={{ alignSelf: "center" }}>{title}</span>
+          </div>
+        );
+      } else if (title === "Javascript") {
+        return (
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <Image
+              style={{ display: "inline-block", margin: "4px" }}
+              src="/images/logo-javascript.svg"
+              alt="javascript logo"
+              width={15}
+              height={15}
+            />
+            {title}
+          </div>
+        );
+      }
+      return <>{title}</>;
+    },
+  },
+  useNextSeoProps() {
+    return {
+      titleTemplate: "%s - OneNode API",
+    };
+  },
+
+  // ... other theme options
+};
